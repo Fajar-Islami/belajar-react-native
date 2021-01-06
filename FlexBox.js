49543b484f73620added36c0c76a38101bb198cf
Fajar-Islami <ahmadfajarislami@protonmail.com>
2021-01-06 17:00:07 +0700

    Video 12 Flexbox

---
+++ FlexBox.js
@@ -0,0 +1,45 @@
+import React, {Component} from 'react';
+import {Image, Text, View} from 'react-native';
+
+export class FlexBox extends Component {
+  render() {
+    return (
+      <View>
+        <View
+          style={{
+            flexDirection: 'row',
+            backgroundColor: '#c8d6e5',
+            alignItems: 'flex-start',
+            justifyContent: 'space-between',
+          }}>
+          <View style={{backgroundColor: '#ee5253', width: 50, height: 50}} />
+          <View style={{backgroundColor: '#feca57', width: 50, height: 50}} />
+          <View style={{backgroundColor: '#1dd1a1', width: 50, height: 50}} />
+          <View style={{backgroundColor: '#5f27cd', width: 50, height: 50}} />
+        </View>
+
+        <View style={{flexDirection: 'row', justifyContent: 'space-around'}}>
+          <Text>Beranda</Text>
+          <Text>Video</Text>
+          <Text>Playlist</Text>
+          <Text>Channel</Text>
+          <Text>Tentang</Text>
+        </View>
+
+        <View
+          style={{flexDirection: 'row', alignItems: 'center', marginTop: 20}}>
+          <Image
+            source={{uri: 'https://placeimg.com/100/100/people'}}
+            style={{width: 100, height: 100, borderRadius: 50, marginRight: 14}}
+          />
+          <View>
+            <Text style={{fontSize: 20, fontWeight: 'bold'}}>Ini Judul</Text>
+            <Text>100 rb subscriber</Text>
+          </View>
+        </View>
+      </View>
+    );
+  }
+}
+
+export default FlexBox;
