95839d95fb70349878882c3da429fbe9fbaad0d5
Fajar-Islami <ahmadfajarislami@protonmail.com>
2021-06-07 13:23:54 +0700

    upload + catatan

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Communication from './pages/Communication';
 import FlexBox from './pages/Flexbox';
 import Position from './pages/Position';
 import PropsDinamis from './pages/PropsDinamis';
+import ReactNativeSvg from './pages/ReactNativeSvg';
 import SampleComponent from './pages/SampleComponent';
 import StateDinamis from './pages/StateDinamis';
 import StylingComponent from './pages/StylingComponent';
@@ -27,7 +28,8 @@ const App = () => {
         {/* <PropsDinamis /> */}
         {/* <StateDinamis /> */}
         {/* <Communication /> */}
-        <BasicJavascript />
+        {/* <BasicJavascript /> */}
+        <ReactNativeSvg />
       </ScrollView>
     </View>
   );
